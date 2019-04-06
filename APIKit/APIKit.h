4ead66f54e0cd2d17268604d870c036eab0b0d68
Roman <misnikovroman@gmail.com>
2019-04-06 10:36:42 +0300

    Initial Commit

---
+++ APIKit/APIKit.h
@@ -0,0 +1,19 @@
+//
+//  APIKit.h
+//  APIKit
+//
+//  Created by MisnikovRoman on 06/04/2019.
+//  Copyright © 2019 MisnikovRoman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APIKit.
+FOUNDATION_EXPORT double APIKitVersionNumber;
+
+//! Project version string for APIKit.
+FOUNDATION_EXPORT const unsigned char APIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIKit/PublicHeader.h>
+
+
